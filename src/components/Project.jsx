6f9b9a4f1112b122dc8dd894175ad9e52fec2cb8
unknown <travihay@gmail.com>
2024-09-30 14:46:43 -0500

    changed the wayt the imgs are being rendered by importing them

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,10 @@
-
 import '../styles/App.css';
+
+// Import all images
+import popcornPicksImage from '../assets/porfolio project.png';
+import artAttackImage from '../assets/Screenshot 2024-08-27 211242.png';
+import forecastTodayImage from '../assets/Screenshot 2024-06-19 153116.png';
+import bloggersApiImage from '../assets/Screenshot 2024-06-09 163256.png';
 
 const Projects = () => {
   return (
@@ -7,19 +12,19 @@ const Projects = () => {
       <h2>Work</h2>
       <div className="work-grid">
         <a className="git-hub" href="https://travishaynie.github.io/popcornpicks2/">
-          <img className="work-img" src="./src/assets/porfolio project.png" alt="Popcorn Picks Project" />
+          <img className="work-img" src={popcornPicksImage} alt="Popcorn Picks Project" />
         </a>
 
         <a className="git-hub" href="https://art-attack-2.onrender.com">
-          <img className="work-img" src="./src/assets/Screenshot 2024-08-27 211242.png" alt="Art Attack Project" />
+          <img className="work-img" src={artAttackImage} alt="Art Attack Project" />
         </a>
 
         <a className="git-hub" href="https://travishaynie.github.io/ForcastToday-Api/">
-          <img className="work-img" src="./src/assets/Screenshot 2024-06-19 153116.png" alt="Forecast Today API" />
+          <img className="work-img" src={forecastTodayImage} alt="Forecast Today API" />
         </a>
 
         <a className="git-hub" href="https://travishaynie.github.io/Bloggers-API/">
-          <img className="work-img" src="./src/assets/Screenshot 2024-06-09 163256.png" alt="Bloggers API" />
+          <img className="work-img" src={bloggersApiImage} alt="Bloggers API" />
         </a>
       </div>
     </section>
