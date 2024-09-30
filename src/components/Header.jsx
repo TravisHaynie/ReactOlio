2e58cdf21c9be1d9cb13cebebeead9de910bc0e0
unknown <travihay@gmail.com>
2024-09-30 14:45:09 -0500

    changed img to import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import '../styles/App.css'; // Ensure the correct path to your CSS file
+import profileImage from '../assets/IMG_0904.JPG'; // Correct way to import the image
 
 const Header = () => {
   return (
     <header>
       <div className="header-content">
-        <img className="photo" src="./src/assets/IMG_0904.JPG" alt="Travis Haynie" /> {/* Add your photo */}
+        <img className="photo" src={profileImage} alt="Travis Haynie" /> {/* Use imported image */}
         <h1>Travis Haynie</h1> {/* Centered title with CSS */}
       </div>
     </header>
